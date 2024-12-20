f196f1d51c45e934bc6ff4a242d76797cdb3073c
Carlos Gutierrez <107132161+carloscuau10@users.noreply.github.com>
2024-12-20 17:23:39 -0500

    Updated from Publii

--- README.md
+++
@@ -1,3 +0,0 @@
-# mercerccm
-Stores website for Mercer University Catholic Campus Ministry
-https://carloscuau10.github.io/mercerccm/home.html
--- navScript.js
+++
@@ -1,9 +0,0 @@
-function navToggle() {
-    var x = document.getElementById("navLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } 
-    else {
-      x.style.display = "block";
-    }
-  }
